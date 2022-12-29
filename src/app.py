fe2b1d3597770761e508af0cf3937fd487d54854
root <root@vps.dxh30.my.id>
2022-12-29 22:38:52 +0530

    Use session instead

--- src/app.py
+++ src/app.py
@@ -5,18 +5,27 @@ import time
 import tweepy
 import json
 
-from flask import Flask, request, redirect, flash
+from flask import Flask, request, redirect, flash, session
+from flask_session import Session
 from dotenv import load_dotenv, find_dotenv
 from jinja2 import Environment, FileSystemLoader
 from faker import Faker
 
 app = Flask(__name__)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 load_dotenv(find_dotenv())
 logged_in = "no"
 time_started = 0
 time_ended = 0
 broadcast = 0
+
+def get_session():
+    if not session.get("name"):
+        return redirect("/login")
+    return session.get("name")
 
 def get_db():
     db = sqlite3.connect('app.db')
@@ -43,7 +52,7 @@ def toggle_broadcast():
     global time_started
     global time_ended
 
-    if logged_in == "no":
+    if not session.get("name"):
         return redirect('/login')
 
     if broadcast == 0:
@@ -57,7 +66,7 @@ def toggle_broadcast():
 
 @app.route('/login')
 def login():
-    if logged_in == "yes":
+    if session.get("name"):
         return redirect('/message_page')
     environment = get_env()
     template = environment.get_template("login.j2")
